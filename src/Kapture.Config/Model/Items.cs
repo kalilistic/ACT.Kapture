2d27c71dd05ed403f9a2c57d9e37af7a2bc344c3
kalilistic <35899782+kalilistic@users.noreply.github.com>
2020-08-01 20:33:02 -0400

    fix more filter bugs

--- src/Kapture.Config/Model/Items.cs
+++ src/Kapture.Config/Model/Items.cs
@@ -5,7 +5,7 @@ namespace ACT_FFXIV_Kapture.Config
 	public class Items
 	{
 		public bool FilterByItems { get; set; } = false;
-		public ItemPreset ItemPreset { get; set; } = ItemPreset.ExcludeCommonItems;
+		public ItemPreset ItemPreset { get; set; } = ItemPreset.Custom;
 		public bool IncludeItems { get; set; }
 		public bool ExcludeItems { get; set; }
 		public List<string> ItemsList { get; set; } = new List<string>();
